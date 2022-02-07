1c2319350664347a9821d8769c3ee5e28490f424
Ronaldinho <73857555+ronaldinhoTEC@users.noreply.github.com>
2022-02-07 15:54:51 -0500

    circulo

--- areacirculo.py
+++ areacirculo.py
@@ -1,9 +1,9 @@
 from math import pi
 
-def areaCirculo(radio, pi):
+def areaCirculo(radio):
 	#radio = int(input(f'Ingresa el valor del circulo'))
-	print(f'El area del circulo es {radio * pi}')
-
+	print(f'El area del circulo es {radio * math.pi}')
+areaCirculo(6)
 
 
 
